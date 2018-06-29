f78289805151d784be1c4bc3aa4e99e0905e09be
Mahmoud Fayed <msfclipper@yahoo.com>
2018-06-29 04:49:43 +0300

    update

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 // include_once("index.html"); 
 
-//system("~/ring/bin/ringlang test.ring") ;
+system("~/ring/bin/ringlang test.ring") ;
 
 //include_once("report.html"); 
 
-system("./~/startapp.sh") ;
+//system("./~/startapp.sh") ;
 
 ?>
