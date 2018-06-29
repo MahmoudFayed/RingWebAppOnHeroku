e3b0fb4280e0bc01d3de37f3c39af81a70218d82
Mahmoud Fayed <msfclipper@yahoo.com>
2018-06-29 04:41:05 +0300

    update

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 
 // include_once("index.html"); 
 
-system("~/ring/bin/ringlang test.ring") ;
+//system("~/ring/bin/ringlang test.ring") ;
 
 //include_once("report.html"); 
+
+system("export LD_LIBRARY_PATH=~/ring/lib && ~/ring/bin/ring test.ring") ;
 
 ?>
