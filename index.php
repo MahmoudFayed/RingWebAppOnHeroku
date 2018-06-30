f2677dc21ddc62482ed8441416861eca1f1f0543
Mahmoud Fayed <msfclipper@yahoo.com>
2018-06-30 10:14:26 +0300

    update

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php 
 
 
-system("~/ring/bin/ringlang test.ring") ;
+//system("~/ring/bin/ringlang test.ring") ;
 
+header("Location: http://www.testring.herokuapp.com/index.ring"); /* Redirect browser */
+exit();
 
 ?>
